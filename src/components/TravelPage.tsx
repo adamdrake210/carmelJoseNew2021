6daf9e10548efcbd125683bdc060c46d0ed14ad0
Adam Drake <addrake@ebay.com>
2021-07-25 15:01:19 +0200

    chore: accommodation and travel page

---
+++ src/components/TravelPage.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import useTranslation from 'next-translate/useTranslation';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    padding: '20px 0px',
+    color: '#fff',
+  },
+  subtitle: {
+    padding: '20px 0px',
+    color: '#fff',
+    textDecoration: 'underline',
+  },
+  text: {
+    padding: '15px 0px 5px',
+    color: '#fff',
+  },
+  boldtext: {
+    padding: '15px 0px 5px',
+    color: '#fff',
+    fontWeight: 700,
+  },
+}));
+
+function Travelpage() {
+  const classes = useStyles();
+  const { t } = useTranslation();
+
+  return (
+    <React.Fragment>
+      <Typography variant="h4" className={classes.title}>
+        {t('travelpage:traveltitle')}
+      </Typography>
+      <Typography variant="h6" className={classes.subtitle}>
+        {t('travelpage:travelnotice')}
+      </Typography>
+      <Typography variant="h5" className={classes.subtitle}>
+        {t('travelpage:flightstitle')}
+      </Typography>
+      <Typography variant="body1" className={classes.text}>
+        {t('travelpage:flightsinfo1')}
+      </Typography>
+      <Typography variant="body1" className={classes.text}>
+        {t('travelpage:flightsinfo2')}
+      </Typography>
+      <Typography variant="h5" className={classes.subtitle}>
+        {t('travelpage:gettingtitle')}
+      </Typography>
+      <Typography variant="body1" className={classes.text}>
+        {t('travelpage:gettinginfo1')}
+      </Typography>
+      <Typography variant="body1" className={classes.text}>
+        {t('travelpage:gettinginfo2')}
+      </Typography>
+      <Typography variant="body1" className={classes.text}>
+        {t('travelpage:gettinginfo3')}
+      </Typography>
+      <Typography variant="body1" className={classes.boldtext}>
+        {t('travelpage:moreinfo')}
+      </Typography>
+    </React.Fragment>
+  );
+}
+
+export default Travelpage;
---
+++ src/pages/accommodation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from 'containers/PageLayout';
+import AccommodationPage from 'components/AccommodationPage';
+
+const Accommodation = () => (
+  <PageLayout>
+    <AccommodationPage />
+  </PageLayout>
+);
+
+export default Accommodation;
